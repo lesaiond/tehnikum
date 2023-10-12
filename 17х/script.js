268f089c2b2f64598ed1f671e9ecce6819bdf45f
Said <1acaunt002@gmail.com>
2023-10-12 18:38:22 +0500

    domaxa

---
+++ 17х/script.js
@@ -0,0 +1,38 @@
+let number = document.querySelector('#number').value
+// let number = '+998998007744'
+let btn = document.querySelector('#btn')
+let text = document.querySelector('#text')
+
+btn.addEventListener('click', function(){
+
+    let list = number.split('');
+    console.log(list)
+    if(list[0] == '+'){
+        list.shift()
+    }else{}
+    if(list[0] == '9' && list[1] == '9' && list[2] == '8' && list.length == 12){
+        alert('We verifed your number')
+        list.splice(0, 0, '+')
+        list.splice(4, 0, '(')
+        list.splice(7, 0, ')')
+        list.splice(11, 0, '-')
+        list.splice(14, 0, '-')
+    }else{
+        alert('Write uz tel. Number')
+    }
+    
+
+    text.innerHTML = list
+    
+})
+
+
+// let test = [123456678111]
+// let splt = test.split('')
+
+// test[0] = ''
+
+
+const readyArray = [45, 12, 54, 23];
+readyArray.splice(2, 0, 60);
+console.log(readyArray);
